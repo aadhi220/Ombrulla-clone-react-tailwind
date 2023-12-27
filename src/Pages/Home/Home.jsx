87ff131742d7d36002ab38179ca6140839d1aece
Adithyan Anil <adithyananil2002@gmail.com>
2023-12-27 11:25:56 +0530

    section-2 added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Hero from "../../Components/Hero/Hero";
 import "./Home.css";
 import Section1 from "../../Components/Sections/Section1";
-
+import Section2 from "../../Components/Sections/Section2";
 function Home() {
   return (
     <>
       <Hero />
 
     <Section1/>
-  
+    <Section2/>
 
 
     </>
