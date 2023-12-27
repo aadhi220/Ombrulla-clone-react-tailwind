2083602daaa7f39d8a2f6aa8fcc5c47402cd5f50
Adithyan Anil <adithyananil2002@gmail.com>
2023-12-27 11:46:45 +0530

    section 3 banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "../../Components/Hero/Hero";
 import "./Home.css";
 import Section1 from "../../Components/Sections/Section1";
 import Section2 from "../../Components/Sections/Section2";
+import Section3banner from "../../Components/Sections/Section3banner";
+
 function Home() {
   return (
     <>
@@ -10,6 +12,8 @@ function Home() {
 
     <Section1/>
     <Section2/>
+    <Section3banner/>
+
 
 
     </>
